e10be20504711e7232905dba43ca2080b4021e38
Vòng Đạt Cường <vongdatcuong280599@gmail.com>
2021-01-22 16:13:50 +0700

    [Change - Search More Page]: filter not found

--- javascript/renderSearchResult.js
+++ javascript/renderSearchResult.js
@@ -7,8 +7,9 @@ $(document).ready(function () {
     const $searchMorePagination = $('#searchMorePagination');
     const $moreResultSearchInp = $('#moreResultSearchInp');
     const $moreResultSearchSubmit = $('#moreResultSearchSubmit');
+    const $filterErrorMsg = $('#filterErrorMsg');
 
-    //$filterFromDate.val((new Date).toISOString().substr(0, 10));
+    $filterFromDate.val((new Date("2020-12-03")).toISOString().substr(0, 10));
     $filterToDate.val((new Date).toISOString().substr(0, 10));
     let key = localStorage.getItem("key");
 
@@ -23,59 +24,6 @@ $(document).ready(function () {
     // 2: Popularity DESC
     let sortOption = 0;
     const allArticles = [
-        {
-            title: "Thông báo đăng ký học phần cho sinh viên bậc Cao đẳng HK1 2020-2021",
-            date: "14/12/2020",
-            summary: "Khoa CNTT thông báo đến tất cả các sinh viên bậc cao đẳng hệ chính quy khóa tuyển 2018 trở về trước về việc đăng ký học phần qua mạng HK1 2020-2021. Sinh viên chú ý theo dõi và đăng ký theo đúng lịch trường đã sắp xếp...",
-            link: "../detailNotiStudentPage/index.html",
-            view: 52626
-        },
-        {
-            title: "Thông báo nộp đơn đăng ký Thực hiện Khóa luận tốt nghiệp/Thực tập tốt nghiệp/Đồ án tốt nghiệp Khóa 2017 - Đợt 2 (Online)",
-            date: "07/12/2020",
-            thumbnail: "../images/detail-notification-image-1.png",
-            summary: "BP Giáo vụ thông báo các bạn sinh viên đã được Giảng viên Doanh nghiệp nhận thực hiện đề tài tốt nghiệp trong đợt đăng ký Thực hiện Khóa luận tốt nghiệp/Thực tập tốt nghiệp/Đồ án tốt nghiệp Khóa 2017 - Đợt 2 ... Chương trình chính quy.",
-            link: "../detailNotiStudentPage/index.html",
-            view: 83636
-        },
-        {
-            title: "Đăng ký ứng tuyển học bổng công ty TNHH ScrumViet",
-            date: "03/12/2020",
-            thumbnail: "../images/detail-notification-image-2.png",
-            summary: "Với mong muốn đóng góp một phần nhỏ vào việc phát triển giáo dục ở Việt Nam và đặc biệt cho ngành công nghệ thông tin - Công ty TNHH ScrumViet.",
-            link: "../detailNotiTeacherPage/index.html",
-            view: 12563
-        },
-        {
-            title: "Thông báo đăng ký học phần cho sinh viên bậc Cao đẳng HK1 2020-2021",
-            date: "03/01/2021",
-            summary: "Khoa CNTT thông báo đến tất cả các sinh viên bậc cao đẳng hệ chính quy khóa tuyển 2018 trở về trước về việc đăng ký học phần qua mạng HK1 2020-2021. Sinh viên chú ý theo dõi và đăng ký theo đúng lịch trường đã sắp xếp...",
-            link: "../detailNotiStudentPage/index.html",
-            view: 9672
-        },
-        {
-            title: "Thông báo nộp đơn đăng ký Thực hiện Khóa luận tốt nghiệp/Thực tập tốt nghiệp/Đồ án tốt nghiệp Khóa 2017 - Đợt 2 (Online)",
-            date: "01/01/2021",
-            thumbnail: "../images/detail-notification-image-1.png",
-            summary: "BP Giáo vụ thông báo các bạn sinh viên đã được Giảng viên Doanh nghiệp nhận thực hiện đề tài tốt nghiệp trong đợt đăng ký Thực hiện Khóa luận tốt nghiệp/Thực tập tốt nghiệp/Đồ án tốt nghiệp Khóa 2017 - Đợt 2 ... Chương trình chính quy.",
-            link: "../detailNotiStudentPage/index.html",
-            view: 2626
-        },
-        {
-            title: "Đăng ký ứng tuyển học bổng công ty TNHH ScrumViet",
-            date: "15/01/2021",
-            thumbnail: "../images/detail-notification-image-2.png",
-            summary: "Với mong muốn đóng góp một phần nhỏ vào việc phát triển giáo dục ở Việt Nam và đặc biệt cho ngành công nghệ thông tin - Công ty TNHH ScrumViet.",
-            link: "../detailNotiTeacherPage/index.html",
-            view: 6361
-        }    ,
-        {
-            title: "Thông báo đăng ký học phần cho sinh viên bậc Cao đẳng HK1 2020-2021",
-            date: "05/01/2021",
-            summary: "Khoa CNTT thông báo đến tất cả các sinh viên bậc cao đẳng hệ chính quy khóa tuyển 2018 trở về trước về việc đăng ký học phần qua mạng HK1 2020-2021. Sinh viên chú ý theo dõi và đăng ký theo đúng lịch trường đã sắp xếp...",
-            link: "../detailNotiStudentPage/index.html",
-            view: 2871
-        },
         {
             title: "Thông báo nộp đơn đăng ký Thực hiện Khóa luận tốt nghiệp/Thực tập tốt nghiệp/Đồ án tốt nghiệp Khóa 2017 - Đợt 2 (Online)",
             date: "03/01/2021",
@@ -232,8 +180,15 @@ $(document).ready(function () {
     }
 
     const filterByDate = (dateStr, fromStr, toStr) => {
-        const from = (fromStr)? (new Date(fromStr)).getTime() : 0;
-        const to = (new Date(toStr)).getTime();
+        let from = 0;
+        if (fromStr){
+            from = new Date(fromStr);
+            from.setHours(0, 0, 0, 0);
+            from = from.getTime();
+        }
+        let to = (new Date(toStr));
+        to.setHours(0, 0, 0, 0);
+        to = to.getTime();
         const today = (new Date(dateStr.replace( /(\d{2})\/(\d{2})\/(\d{4})/, "$2/$1/$3"))).getTime();
         return today >= from && today <= to;
 
@@ -260,32 +215,38 @@ $(document).ready(function () {
     })
 
     const renderResult = (articles, keyword) => {
-        $searchResultList.empty();
-        for (let i = page * perPage, count = 0; i < articles.length && count < perPage; i++, count++){
-            let article = articles[i];
-            const keywordRegex = new RegExp(keyword, "gi");
-            let title = article.title.replace(keywordRegex, (matching) => {
-                return `<span class="searchResult-number" style="color: red; font-style: italic;"><u>${matching}&nbsp;</u></span>`
-            });
-            const $li = $('<li class="py-4"></li>');
-            const $h6 = $(`<h6 class="article-title"></h6>`);
-            const $h6a = $(`<a href=${article.link}>${title}</a>`);
-            $h6.append($h6a);
-            $li.append($h6);
+        if (articles.length == 0){
+            $filterErrorMsg.show();
+            $searchResultList.empty();
+        } else {
+            $filterErrorMsg.hide();
+            $searchResultList.empty();
+            for (let i = page * perPage, count = 0; i < articles.length && count < perPage; i++, count++){
+                let article = articles[i];
+                const keywordRegex = new RegExp(keyword, "gi");
+                let title = article.title.replace(keywordRegex, (matching) => {
+                    return `<span class="searchResult-number" style="color: red; font-style: italic;"><u>${matching}&nbsp;</u></span>`
+                });
+                const $li = $('<li class="py-4"></li>');
+                const $h6 = $(`<h6 class="article-title"></h6>`);
+                const $h6a = $(`<a href=${article.link}>${title}</a>`);
+                $h6.append($h6a);
+                $li.append($h6);
 
-            const $row = $('<div class="row"></div>');
-            const $date = $(`<div class="col-md-2 article-date my-0" style="font-size: 0.9rem; color: rgba(0,0,0,0.5)"><u>${article.date}</u></div>`);
-            if (article.thumbnail){
-                const $thumbnail = $(`<div class="search-article-thumbnail"><img src="${article.thumbnail}" /></div>`);
-                $date.prepend($thumbnail);
-            }
-            const $content = $(`<div class="col-md-10 article-content py-2" style="font-size: 0.9rem; color: rgba(0,0,0,0.5)"><p>${article.summary}</p></div>`)
-            $row.append($date, $content);
+                const $row = $('<div class="row"></div>');
+                const $date = $(`<div class="col-md-2 article-date my-0" style="font-size: 0.9rem; color: rgba(0,0,0,0.5)"><u>${article.date}</u></div>`);
+                if (article.thumbnail){
+                    const $thumbnail = $(`<div class="search-article-thumbnail"><img src="${article.thumbnail}" /></div>`);
+                    $date.prepend($thumbnail);
+                }
+                const $content = $(`<div class="col-md-10 article-content py-2" style="font-size: 0.9rem; color: rgba(0,0,0,0.5)"><p>${article.summary}</p></div>`)
+                $row.append($date, $content);
 
-            $li.append($row);
-            $searchResultList.append($li);
-        }        
-        renderPagination(page, displayedArtciles);
+                $li.append($row);
+                $searchResultList.append($li);
+            }        
+            renderPagination(page, displayedArtciles);
+        }
     }
 
     $('#sortByDateDesc').on('click', (e) => {
@@ -328,7 +289,7 @@ $(document).ready(function () {
 
     const renderPagination = (localPage, data) => {
         $searchMorePagination.empty();
-        const count = Math.floor(data.length / perPage) + 1;
+        const count = Math.ceil(data.length / perPage);
         const $first = $('<a href="#"><li><<</li></a>');
         $first.on('click', (e) => {
             page = 0;
